41ed8a0ff58b2c36b14fa8060f835f404ef05896
myasnick-off <cyber.jokep@gmail.com>
2021-08-29 21:04:42 +0300

    Test App for launching Calculator

--- app/src/main/java/com/example/startingapp/MainActivity.java
+++ app/src/main/java/com/example/startingapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.startingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri calcUri = Uri.parse("scheme://calculator");
+
+                Intent launchIntent = new Intent(Intent.ACTION_VIEW, calcUri);
+                Intent chooser = Intent.createChooser(launchIntent, "Start calc");
+                startActivity(chooser);
+
+            }
+        });
     }
 }
